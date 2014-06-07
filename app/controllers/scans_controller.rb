9305bd758d018c9dacd5fd197bebc8b249d4fdc8
nosolopau <pau@nosolopau.com>
2014-06-07 18:47:32 +0200

    Using bootstrap with less

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,11 +1,5 @@
 class ScansController < ApplicationController
-  before_action :set_scan, only: [:show, :edit, :update, :destroy]
-
-  # GET /scans
-  # GET /scans.json
-  def index
-    @scans = Scan.all
-  end
+  before_action :set_scan, only: [:show]
 
   # GET /scans/1
   # GET /scans/1.json
@@ -15,10 +9,6 @@ class ScansController < ApplicationController
   # GET /scans/new
   def new
     @scan = Scan.new
-  end
-
-  # GET /scans/1/edit
-  def edit
   end
 
   # POST /scans
@@ -34,30 +24,6 @@ class ScansController < ApplicationController
         format.html { render :new }
         format.json { render json: @scan.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /scans/1
-  # PATCH/PUT /scans/1.json
-  def update
-    respond_to do |format|
-      if @scan.update(scan_params)
-        format.html { redirect_to @scan, notice: 'Scan was successfully updated.' }
-        format.json { render :show, status: :ok, location: @scan }
-      else
-        format.html { render :edit }
-        format.json { render json: @scan.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /scans/1
-  # DELETE /scans/1.json
-  def destroy
-    @scan.destroy
-    respond_to do |format|
-      format.html { redirect_to scans_url, notice: 'Scan was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
