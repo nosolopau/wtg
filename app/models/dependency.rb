505ce75a958671f35e8a39c299a0c22c271f06b6
nosolopau <pau@nosolopau.com>
2014-07-28 18:51:40 +0200

    Adding gem info to dependencies

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -8,11 +8,31 @@ class Dependency
   field :source, type: String
   field :groups, type: Array
 
+  field :info
+  field :licenses
+  field :project_uri
+  field :current_version
+
+  field :status
+
   belongs_to :scan
 
   def self.create_from_gemfile(gemfile)
     Gemnasium::Parser.gemfile(gemfile).dependencies.map do |dependency|
-      self.create(groups: dependency.groups, source: dependency.source, kind: dependency.type, name: dependency.name, requirement: dependency.requirement)
+      new_dependency = self.create(groups: dependency.groups, source: dependency.source, kind: dependency.type, name: dependency.name, requirement: dependency.requirement)
+      new_dependency.retrieve_and_set_details!
+      new_dependency
     end
+  end
+
+  def retrieve_and_set_details!
+    gem_info = Gems.info(self.name)
+
+    self.licenses = gem_info['licenses'].join(',')
+    self.info = gem_info['info']
+    self.current_version = gem_info['version']
+    self.project_uri = gem_info['project_uri']
+
+    self.save
   end
 end
