86980a911cef8be152c6890fd6cc48154c6f5890
nosolopau <pau@nosolopau.com>
2015-01-11 00:26:54 +0100

    Fixed deployment envs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '128.199.59.122', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '128.199.59.122', user: 'deploy', roles: %w{web app}
 
 
 # Custom SSH Options
